168658bff50f895ab71edbecc1f2451ede4014f6
joao tiago viegas <joao.viegas@maersk.com>
2019-11-18 00:37:53 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/main.tf
+++ modules/entities-lambda-endpoint/main.tf
@@ -22,10 +22,10 @@ resource "aws_api_gateway_method" "resources-methods" {
   authorization = "NONE"
 }
 resource "aws_api_gateway_integration" "lambda-integration-resources" {
-  for_each                  = aws_api_gateway_resource.resources
+  for_each                  = values(aws_api_gateway_resource.resources)
   rest_api_id               = "${aws_api_gateway_rest_api.api.id}"
   resource_id               = each.value.id
-  http_method               = values(aws_api_gateway_method.resources-methods)[each.index].http_method
+  http_method               = values(aws_api_gateway_method.resources-methods)[each.key].http_method
   integration_http_method   = "POST"
   type                      = "AWS_PROXY"
   uri                       = "${aws_lambda_function.lambda.invoke_arn}"
