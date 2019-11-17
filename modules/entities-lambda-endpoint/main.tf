5e86fb6b2322a1797d86516d8157391cae557160
joao tiago viegas <joao.viegas@maersk.com>
2019-11-17 23:54:32 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/main.tf
+++ modules/entities-lambda-endpoint/main.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resources" {
-  for_each      = local.resources
+  for_each      = toset(local.resources)
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   parent_id     = "${aws_api_gateway_rest_api.api.root_resource_id}"
   path_part     = each.value
