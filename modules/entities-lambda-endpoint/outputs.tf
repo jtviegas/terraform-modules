76f9dd56278ed40bfc36b0f2520b3bf14036eb61
joao tiago viegas <joao.viegas@maersk.com>
2019-11-18 00:10:09 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/outputs.tf
+++ modules/entities-lambda-endpoint/outputs.tf
@@ -1,19 +1,19 @@
 
 output "resource-id" {
-  value       = aws_api_gateway_resource.resource.id
+  value       = values(aws_api_gateway_resource.resources)[*].id
 }
 
 output "resource-path" {
-  value       = aws_api_gateway_resource.resource.path
+  value       = values(aws_api_gateway_resource.resources)[*].path
 }
 
 output "invoke-url" {
-    value       = aws_api_gateway_deployment.deployment.invoke_url
+    value       = values(aws_api_gateway_deployment.deployment-resources)[*].invoke_url
     description = "The URL to invoke the API pointing to the stage, e.g. https://z4675bid1j.execute-api.eu-west-2.amazonaws.com/prod"
 }
 
 output "execution-arn" {
-    value       = aws_api_gateway_deployment.deployment.execution_arn
+    value       = values(aws_api_gateway_deployment.deployment)[*].execution_arn
     description = "The execution ARN to be used in lambda_permission's source_arn when allowing API Gateway to invoke a Lambda function, e.g. arn:aws:execute-api:eu-west-2:123456789012:z4675bid1j/prod"
 }
 
