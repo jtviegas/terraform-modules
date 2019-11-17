c69c4866b3dfb7b98738dfe9edc5857e9556b44e
joao tiago viegas <joao.viegas@maersk.com>
2019-11-17 21:16:03 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/main.tf
+++ modules/entities-lambda-endpoint/main.tf
@@ -32,7 +32,8 @@ resource "aws_api_gateway_integration" "lambda-integration-resources" {
 }
 
 resource "aws_api_gateway_deployment" "deployment-resources" {
-  depends_on = toset(values(aws_api_gateway_integration.lambda-integration-resources))
+  for_each      = aws_api_gateway_integration.lambda-integration-resources
+  depends_on = [ each.value ]
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.environment}"
