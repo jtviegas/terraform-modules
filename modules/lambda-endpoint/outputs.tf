82630c6b0160559aeb5f7ffc4fe5cf99e56407e4
joao tiago viegas <joao.viegas@maersk.com>
2019-11-17 18:23:19 +0100

    bugfix there

--- modules/lambda-endpoint/outputs.tf
+++ modules/lambda-endpoint/outputs.tf
@@ -8,7 +8,7 @@ output "resource-path" {
 }
 
 output "invoke-url" {
-    value       = aws_api_gateway_resource.resource.path
+    value       = aws_api_gateway_deployment.deployment.invoke_url
     description = "The URL to invoke the API pointing to the stage, e.g. https://z4675bid1j.execute-api.eu-west-2.amazonaws.com/prod"
 }
 
