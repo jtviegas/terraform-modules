ac093adca80c3bc051ad06dd8b9590f835cac477
joao tiago viegas <joao.viegas@maersk.com>
2019-11-18 11:21:01 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/main.tf
+++ modules/entities-lambda-endpoint/main.tf
@@ -27,15 +27,15 @@ resource "aws_api_gateway_integration" "lambda-integration-resources" {
   for_each                  = toset(local.resources)
   rest_api_id               = "${aws_api_gateway_rest_api.api.id}"
   resource_id               = "${aws_api_gateway_resource.resources[each.key].id}"
-  http_method               = "${aws_api_gateway_resource.resources[each.key].http_method}"[each.key].http_method
+  http_method               = "${aws_api_gateway_method.resources-methods[each.key].http_method}"
   integration_http_method   = "POST"
   type                      = "AWS_PROXY"
   uri                       = "${aws_lambda_function.lambda.invoke_arn}"
 }
 
 resource "aws_api_gateway_deployment" "deployment-resources" {
-
-  depends_on    = [ aws_api_gateway_integration.lambda-integration-resources ]
+  for_each      = toset(local.resources)
+  depends_on    = [ "${aws_api_gateway_integration.lambda-integration-resources[each.key]}" ]
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "${var.environment}"
 
