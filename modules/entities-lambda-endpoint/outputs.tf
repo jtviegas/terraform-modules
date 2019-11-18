fb09bd290026da886f54f3845bae7d4a86be0d49
joao tiago viegas <joao.viegas@maersk.com>
2019-11-18 14:36:10 +0100

    fighting with loops

--- modules/entities-lambda-endpoint/outputs.tf
+++ modules/entities-lambda-endpoint/outputs.tf
@@ -4,6 +4,7 @@ output "entity-invoke-url" {
     value       = aws_api_gateway_deployment.deployment-resources-entity.invoke_url
     description = "The URL to invoke the API pointing to the stage, e.g. https://z4675bid1j.execute-api.eu-west-2.amazonaws.com/prod"
 }
+
 output "id-invoke-url" {
     value       = aws_api_gateway_deployment.deployment-resources-id.invoke_url
     description = "The URL to invoke the API pointing to the stage, e.g. https://z4675bid1j.execute-api.eu-west-2.amazonaws.com/prod"
