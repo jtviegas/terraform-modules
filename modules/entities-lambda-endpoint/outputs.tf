cda6c41dfe083d1f1509d5069e8b8fd228fb6abe
joao tiago viegas <joao.viegas@maersk.com>
2019-11-18 16:19:28 +0100

    tweaking...

--- modules/entities-lambda-endpoint/outputs.tf
+++ modules/entities-lambda-endpoint/outputs.tf
@@ -3,7 +3,3 @@ output "invoke-url" {
     value       = aws_api_gateway_deployment.deployment-resources-entity.invoke_url
     description = "The URL to invoke the API pointing to the stage, e.g. https://z4675bid1j.execute-api.eu-west-2.amazonaws.com/prod"
 }
-
-output "tables-arn" {
-  value       = module.tables.arns
-}
