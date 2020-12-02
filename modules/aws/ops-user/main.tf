ed8a08195b6f407415f0a5294a1eaf919e8171b6
joao tiago viegas <jtviegas.js@gmail.com>
2020-12-02 23:48:50 +0100

    new ops-user module

--- modules/aws/ops-user/main.tf
+++ modules/aws/ops-user/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "aws_iam_user" "user_name" {
   name = var.user_name
 }
---
+++ test/aws/ops-user/main.tfvars
@@ -0,0 +1,4 @@
+region = "eu-west-1"
+user_public_key = "keybase:jtviegas"
+group_name = "devops_test"
+user_name = "root"
